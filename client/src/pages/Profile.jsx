c4432ee7420998bd515fe2d1d5c12e384649a170
elvintern <elvintern@gmail.com>
2023-03-06 21:10:18 +1300

    Display userInfo

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -6,7 +6,7 @@ import './Profile.scss';
 const Profile = () => {
   const { userId } = useParams();
   const navigate = useNavigate();
-  const [userInfo, setUserInfo] = useState({});
+  const [userInfo, setUserInfo] = useState(null);
 
   useEffect(() => {
     const fetchUserInfo = async () => {
@@ -20,11 +20,10 @@ const Profile = () => {
           },
         }
       );
-
       const json = await response.json();
+      console.log(json);
       setUserInfo(json);
     };
-
     fetchUserInfo();
   }, []);
 
@@ -35,22 +34,25 @@ const Profile = () => {
   return (
     <>
       <Quote />
-      {!userInfo.email ? (
+      {!userInfo ? (
         <p>loading...</p>
       ) : (
         <div className="profile">
           <p className="profile__welcome-msg">welcome {userInfo.firstName}</p>
           <p className="profile__report-times">
             you have been to the gym {userInfo.totalCount} time
-            {userInfo.totalCount > 1 && `s`} since {userInfo.joiningDate}
+            {userInfo.totalCount > 1 && `s`} since{' '}
+            {userInfo.created_at.substring(0, 10)}
           </p>
           <p className="profile__user-goal">your goal: {userInfo.goal}</p>
-          <p>{JSON.stringify(userInfo.routines.routineName)}</p>
         </div>
       )}
 
       <select name="routine" className="user-routines">
         <option defaultChecked>choose your workout routine</option>
+        {userInfo.routines.map((routine) => {
+          return <option>{routine.routineName}</option>;
+        })}
       </select>
 
       <Link className="btn btn--top" to="/exercise">
