97c2632480f3b3b2da72ee5b116899e093612964
elvintern <elvintern@gmail.com>
2023-03-09 15:03:08 +1300

    Add store newUserRoutine function

--- client/src/pages/Routine.jsx
+++ client/src/pages/Routine.jsx
@@ -4,19 +4,26 @@ import { Link, useParams } from 'react-router-dom';
 
 export default function Routine() {
   const { userId } = useParams();
-  const [userRoutine, setUserRoutine] = useState({});
-  const [routine, setRoutine] = useState([]);
+  const [routineName, setRoutineName] = useState('');
   const [exercise, setExercise] = useState('');
+  const [exercises, setExercises] = useState([]);
+  const [newRoutine, setNewRoutine] = useState([]);
 
   function handleSubmit(event) {
     event.preventDefault();
-    const newRoutine = [...routine, exercise];
-    setRoutine(newRoutine);
+    const userRoutine = { routineName, exercises };
+    console.log(userRoutine);
+    setNewRoutine((prev) => [...prev, userRoutine]);
+    setRoutineName('');
     setExercise('');
+    console.log(newRoutine);
   }
 
-  function handleChange(event) {
-    setExercise(event.target.value);
+  function addExercise(event) {
+    event.preventDefault();
+    setExercises((prev) => [...prev, exercise]);
+    console.log(exercises);
+    setExercise('');
   }
 
   useEffect(() => {
@@ -25,17 +32,17 @@ export default function Routine() {
 
   return (
     <>
-      <form className="form form-Routine" onSubmit={handleSubmit}>
-        <label htmlFor="userRoutine" className="form__label">
+      <form className="form form-Routine">
+        <label htmlFor="routineName" className="form__label">
           routine name
         </label>
         <input
           type="text"
-          name="userRoutine"
+          name="routineName"
           className="form__input"
           placeholder="ex) Back Day, Leg Day..."
-          value={exercise}
-          onChange={handleChange}
+          value={routineName}
+          onChange={(e) => setRoutineName(e.target.value)}
         />
         <label htmlFor="userRoutine" className="form__label">
           add your exercise
@@ -46,16 +53,22 @@ export default function Routine() {
           className="form__input"
           placeholder="ex) Deadlift, Squat ..."
           value={exercise}
-          onChange={handleChange}
+          onChange={(e) => setExercise(e.target.value)}
         />
-        {routine.map((el) => {
-          return <p key={Number(Date.now)}>{el}</p>;
+        {exercises.map((el) => {
+          return <p key={el}>{el}</p>;
         })}
 
-        <button className="btn btn-add">Add</button>
+        <button onClick={addExercise} className="btn btn-add">
+          Add
+        </button>
 
-        <Link to="/profile" className="btn btn-signup">
+        <button onClick={handleSubmit} className="btn btn-signup">
           save
+        </button>
+
+        <Link to={`/profile/${userId}`} className="btn btn-signup">
+          Go Back to Main
         </Link>
       </form>
     </>
