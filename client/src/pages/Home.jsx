8df7712fff09e9c55c137377e7d6009aa2c0b416
elvintern <elvintern@gmail.com>
2023-03-09 16:33:32 +1300

    Remove import Quote component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Login from '../components/Login';
-import Quote from '../components/Quote';
 import './Home.scss';
 
 export default function Home() {
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, Link, useNavigate } from 'react-router-dom';
-import Quote from '../components/Quote';
 import './Profile.scss';
 
 const Profile = () => {
