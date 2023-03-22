14c30e07311086a8931e0818ca897d9e67118718
elvintern <elvintern@gmail.com>
2023-03-23 12:08:08 +1300

    Add passing routine ID to workout page function and Validation check on Work Out button

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,21 +1,38 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, Link, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import './Profile.scss';
 import fetchUserInfoById from '../utils/index.js';
+import ValidCheck from '../components/ValidCheck';
 
 const Profile = () => {
   const { userId } = useParams();
   const navigate = useNavigate();
   const [userInfo, setUserInfo] = useState(null);
+  const [selectedRoutine, setSelectedRoutine] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
   useEffect(() => {
-    fetchUserInfoById(userId).then((res) => {
-      setUserInfo(res);
-    });
-  }, []);
+    async function getUserInfo() {
+      try {
+        const res = await fetchUserInfoById(userId);
+        setUserInfo(res);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    getUserInfo();
+  }, [userId]);
 
-  const manageRoutines = () => {
-    navigate(`routine`);
+  const startWorkOut = () => {
+    const Routine = userInfo.routines.find(
+      (routine) => routine.routineName === selectedRoutine
+    );
+
+    if (selectedRoutine) {
+      navigate(`workout/${Routine._id}`);
+    } else {
+      setIsValid(false);
+    }
   };
 
   return (
@@ -34,25 +51,32 @@ const Profile = () => {
             <p className="profile__user-goal">your goal: {userInfo.goal}</p>
           </div>
 
-          <select name="routine" className="user-routines">
+          <select
+            name="routine"
+            className="user-routines"
+            value={selectedRoutine}
+            onChange={(e) => setSelectedRoutine(e.target.value)}
+          >
             <option defaultChecked>choose your workout routine</option>
             {userInfo.routines.length > 0 &&
               userInfo.routines.map((routine) => {
-                return (
-                  <option key={routine.routineName}>
-                    {routine.routineName}
-                  </option>
-                );
+                return <option key={routine._id}>{routine.routineName}</option>;
               })}
           </select>
         </>
       )}
 
-      <Link className="btn btn--top" to="/exercise">
-        start work out
-      </Link>
+      {!isValid && (
+        <ValidCheck
+          isValid={isValid}
+          message={'You have to choose your routine!'}
+        />
+      )}
 
-      <button className="btn btn--bottom" onClick={manageRoutines}>
+      <button className="btn btn--top" onClick={startWorkOut}>
+        start work out
+      </button>
+      <button className="btn btn--bottom" onClick={() => navigate(`routine`)}>
         manage routines
       </button>
     </>
