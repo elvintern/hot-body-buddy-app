40b7498025499c5871269d2345aff0648d8bbbbe
elvintern <elvintern@gmail.com>
2023-03-27 19:26:02 +1300

    Add conditional rendering function

--- client/src/pages/Workout.jsx
+++ client/src/pages/Workout.jsx
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { fetchRoutineById } from '../utils/index.js';
 import Exercise from '../components/Exercise.jsx';
+import WorkoutResult from '../components/WorkoutResult.jsx';
 
 export default function Workout() {
   const { userId, workoutId } = useParams();
   const [routine, setRoutine] = useState(null);
   const [performance, setPerformance] = useState([]);
+  const [isFinished, setisFinished] = useState(false);
 
   useEffect(() => {
     async function getRoutine() {
@@ -20,21 +22,37 @@ export default function Workout() {
     getRoutine();
   }, [workoutId]);
 
+  const finishWorkout = () => {
+    setisFinished(true);
+  };
+
   return (
     <div className="workout">
-      {routine && (
-        <>
-          <h2 className="heading heading-secondary">{routine.routineName}</h2>
-          {routine.exercises.map((exercise, index) => {
-            return (
-              <div key={index} className="workout-form">
-                <ul className="exercises">
-                  <Exercise exercise={exercise} />
-                </ul>
-              </div>
-            );
-          })}
-        </>
+      {isFinished ? (
+        <WorkoutResult routine={routine} />
+      ) : (
+        routine && (
+          <>
+            <h2 className="heading heading-secondary">{routine.routineName}</h2>
+            {routine.exercises.map((exercise, index) => {
+              return (
+                <div key={index} className="workout-form">
+                  <ul className="exercises">
+                    <Exercise
+                      exercise={exercise}
+                      performance={performance}
+                      setPerformance={setPerformance}
+                    />
+                  </ul>
+                </div>
+              );
+            })}
+
+            <button onClick={finishWorkout} className="btn btn--workout">
+              Finish Workout
+            </button>
+          </>
+        )
       )}
     </div>
   );
