900e829a69a2273aa396c68c0b06e5c4eb11bac0
elvintern <elvintern@gmail.com>
2023-04-09 16:51:34 +1200

    Add directing user function in Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Login from '../components/Login';
 import './Home.scss';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../components/AuthContext';
 
 export default function Home() {
+  const userId = localStorage.getItem('id');
+  const navigate = useNavigate();
+  const { isLoggedIn, login } = useContext(AuthContext);
+  if (userId && isLoggedIn) {
+    login();
+    navigate(`/profile/${userId}`);
+  }
+
   return <Login />;
 }
