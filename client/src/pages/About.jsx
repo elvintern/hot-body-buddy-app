46c5c0a38f79873cd180afbfde7e00627f5dfdb3
elvintern <elvintern@gmail.com>
2023-04-10 23:59:25 +1200

    Add content for about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="about">
+      <h2 className="heading-secondary">Welcome to Hot Body Buddy!!</h2>
+      <p className="paragraph">
+        the ultimate fitness SPA designed to help you achieve your health goals
+        and make exercising a regular habit! With your Hot body buddy, you can
+        customize your workout routines, track your progress, and receive
+        motivational quotes and tips to keep you motivated every step of the
+        way. Our user-friendly interface allows you to easily access your
+        profile where you can view your goals and see how many times you have
+        visited the gym. Plus, with our performance tracker, you can compare
+        your progress over time, seeing how far you've come and how much you've
+        achieved. Hot Body Buddy is your perfect companion for a healthier,
+        happier you!
+      </p>
+    </div>
+  );
 }
