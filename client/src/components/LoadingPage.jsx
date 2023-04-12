97daaafb07a151650d6c2de93834171cb25b3f5e
elvintern <elvintern@gmail.com>
2023-04-12 21:07:50 +1200

    Add random loading image function

--- client/src/components/LoadingPage.jsx
+++ client/src/components/LoadingPage.jsx
@@ -2,6 +2,24 @@ import React, { useState, useEffect } from 'react';
 
 function LoadingPage() {
   const [isLoading, setIsLoading] = useState(true);
+  const [loadingImage, setLoadingImage] = useState(null);
+
+  useEffect(() => {
+    if (window.innerWidth >= 992) {
+      console.log(Math.floor(Math.random() * landscape.length));
+      setLoadingImage(landscape[Math.floor(Math.random() * landscape.length)]);
+    } else {
+      console.log(Math.floor(Math.random() * portrait.length));
+      setLoadingImage(portrait[Math.floor(Math.random() * portrait.length)]);
+    }
+
+    const timer = setTimeout(() => {
+      console.log('change');
+      setIsLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   const landscape = Object.values(
     import.meta.glob(
@@ -22,27 +40,15 @@ function LoadingPage() {
     )
   );
 
-  console.log(portrait, landscape);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      console.log('change');
-      setIsLoading(false);
-    }, 2000);
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <>
       {isLoading ? (
         <div
           className="loading-image"
           style={{
-            background: `url('${portrait[1]}') center/cover`,
+            background: `url('${loadingImage}') center/cover`,
           }}
-        >
-          hello
-        </div>
+        ></div>
       ) : (
         <p>Content loaded!</p>
       )}
