1386e93717496a7bd070771b09aa9e08ced1f189
elvintern <elvintern@gmail.com>
2023-04-21 16:55:14 +1200

    Add styling to workout page and increase font-size for headings

--- client/src/pages/Workout.jsx
+++ client/src/pages/Workout.jsx
@@ -7,6 +7,7 @@ import {
 } from '../utils/index.js';
 import Exercise from '../components/Exercise.jsx';
 import WorkoutResult from '../components/WorkoutResult.jsx';
+import './Workout.scss';
 
 export default function Workout() {
   const { userId, workoutId } = useParams();
@@ -39,39 +40,41 @@ export default function Workout() {
 
   return (
     <div className="workout">
-      {isFinished ? (
-        <>
-          <WorkoutResult routine={routine} performance={performance} />
-          <button onClick={finishReview} className="btn">
-            End Review
-          </button>
-        </>
-      ) : (
-        routine && (
+      <div className="workout__container">
+        {isFinished ? (
           <>
-            <h2 className="heading heading--secondary">
-              {routine.routineName}
-            </h2>
-            {routine.exercises.map((exercise, index) => {
-              return (
-                <div key={index} className="workout-form">
-                  <ul className="exercises">
-                    <Exercise
-                      exercise={exercise}
-                      performance={performance}
-                      setPerformance={setPerformance}
-                    />
-                  </ul>
-                </div>
-              );
-            })}
-
-            <button onClick={finishWorkout} className="btn btn--workout">
-              Finish Workout
+            <WorkoutResult routine={routine} performance={performance} />
+            <button onClick={finishReview} className="btn">
+              End Review
             </button>
           </>
-        )
-      )}
+        ) : (
+          routine && (
+            <>
+              <h2 className="heading heading--secondary">
+                {routine.routineName}
+              </h2>
+              {routine.exercises.map((exercise, index) => {
+                return (
+                  <div key={index} className="workout-form">
+                    <ul className="exercises">
+                      <Exercise
+                        exercise={exercise}
+                        performance={performance}
+                        setPerformance={setPerformance}
+                      />
+                    </ul>
+                  </div>
+                );
+              })}
+
+              <button onClick={finishWorkout} className="btn btn--workout">
+                Finish Workout
+              </button>
+            </>
+          )
+        )}
+      </div>
     </div>
   );
 }
