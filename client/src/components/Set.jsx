cca0a3a9ae380e7bd87faa43753c10f739d74274
elvintern <elvintern@gmail.com>
2023-04-21 16:56:54 +1200

    Add styling for set component

--- client/src/components/Set.jsx
+++ client/src/components/Set.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import './Set.scss';
 
 export default function Set(props) {
   const [record, setRecord] = useState({
@@ -33,35 +34,42 @@ export default function Set(props) {
   };
 
   return (
-    <li className="exercise">
-      <p className="workout-form__set">{props.sets} set</p>
-      <label htmlFor="reps" className="workout-form__label">
-        Reps
-      </label>
-      <input
-        onChange={(e) => handleChange(e)}
-        type="number"
-        name="reps"
-        id="reps"
-        className="workout-form__input"
-      />
-      <label htmlFor="weight" className="workout-form__label">
-        Weight
-      </label>
-      <input
-        onChange={(e) => handleChange(e)}
-        type="number"
-        name="weight"
-        id="weight"
-        placeholder="kgs"
-        className="workout-form__input"
-      />
-      <button onClick={addSet} className="btn btn--set">
-        +
-      </button>
-      <button onClick={deleteSet} className="btn btn--set">
-        -
-      </button>
+    <li className="set">
+      <p className="set__count">{`${props.sets}set`}</p>
+
+      <div className="form__container form__container--set">
+        <label htmlFor="reps" className="form__label form__label--set">
+          Reps
+        </label>
+        <input
+          onChange={(e) => handleChange(e)}
+          type="number"
+          name="reps"
+          id="reps"
+          className="form__input"
+        />
+      </div>
+      <div className="form__container form__container--set">
+        <label htmlFor="weight" className="form__label form__label--set">
+          Weight
+        </label>
+        <input
+          onChange={(e) => handleChange(e)}
+          type="number"
+          name="weight"
+          id="weight"
+          placeholder="kgs"
+          className="form__input"
+        />
+      </div>
+      <div className="form__container form__container--set">
+        <button onClick={addSet} className="btn btn--set">
+          +
+        </button>
+        <button onClick={deleteSet} className="btn btn--set">
+          -
+        </button>
+      </div>
     </li>
   );
 }
