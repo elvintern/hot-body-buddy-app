58d429473ff68646461fb457d54dcd19c0fe098a
elvintern <elvintern@gmail.com>
2023-04-22 11:56:06 +1200

    Add check icon for done exercise function

--- client/src/components/Exercise.jsx
+++ client/src/components/Exercise.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import Set from './Set.jsx';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDumbbell } from '@fortawesome/free-solid-svg-icons';
+import { faDumbbell, faCheck } from '@fortawesome/free-solid-svg-icons';
+import './Exercise.scss';
 
 export default function Exercise({ exercise, performance, setPerformance }) {
   const [workoutResult, setWorkoutResult] = useState({
@@ -61,15 +62,26 @@ export default function Exercise({ exercise, performance, setPerformance }) {
   };
 
   return (
-    <div className="exercise" style={isDone ? { display: 'none' } : null}>
+    <div className="exercise">
       <h3 className="heading heading--tertiary">
         <FontAwesomeIcon className="exercise__icon" icon={faDumbbell} />
-        {exercise}
+        {exercise}{' '}
+        {isDone ? (
+          <FontAwesomeIcon
+            className="exercise__icon exercise__icon--check"
+            icon={faCheck}
+          />
+        ) : null}
       </h3>
-      {renderComponents()}
-      <button onClick={handleClick} className="btn">
-        done
-      </button>
+      <div
+        className="exercise__container"
+        style={isDone ? { display: 'none' } : null}
+      >
+        {renderComponents()}
+        <button onClick={handleClick} className="btn">
+          done
+        </button>
+      </div>
     </div>
   );
 }
