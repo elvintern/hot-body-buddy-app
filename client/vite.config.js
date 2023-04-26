4ce13007b950c44d4467b33322f6cefc31b694f4
elvintern <elvintern@gmail.com>
2023-04-26 21:10:32 +1200

    Add vite plugin for png files

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import imagemin from 'vite-plugin-imagemin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    imagemin({
+      gifsicle: {},
+      mozjpeg: {},
+      pngquant: {
+        quality: [0.7, 0.9],
+        speed: 4,
+      },
+      svgo: {},
+      webp: {},
+    }),
+  ],
+  assetsInclude: ['**/*.PNG'],
 });
